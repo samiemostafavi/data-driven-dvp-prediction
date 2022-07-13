015d0142ac0ddb8ef947a6172c7563c4bb76d43d
Samie Mostafavi <ssmos@kth.se>
2022-07-13 17:06:27 +0200

    Re-factored 2_train_predictors.py with json and dict

--- 2_train_predictors.py
+++ 2_train_predictors.py
@@ -7,6 +7,7 @@ from petastorm import TransformSpec
 import os
 from os.path import dirname, abspath
 from loguru import logger
+import json
 from petastorm.spark import SparkDatasetConverter, make_spark_converter
 from pr3d.de import ConditionalGammaEVM, ConditionalGaussianMM, ConditionalGammaMixtureEVM
 import warnings
@@ -36,32 +37,55 @@ npdtype = np.float64
 tfdtype = tf.float64
 strdtype = 'float64'
 
+training_params = {
+    'dataset_size': 60*1024*1024,
+    'batch_size': 1024*240,
+    'train_val_split': {
+        'fraction': [0.99, 0.01],
+        'seed': 12345,
+    },
+    'rounds' : [
+        {'learning_rate': 0.01, 'epochs':10},
+        {'learning_rate': 0.001, 'epochs':20},
+    ],
+}
+
 condition_labels = ['queue_length', 'longer_delay_prob']
 y_label = 'end2end_delay'
 
-model_names = ['gmm','gmevm']
-ensembles = 1
-
-dataset_size = 60*1024*1024 # total will be the_number_of_files * file_samples_size
-batch_size = 1024*240
-train_val_split = {
-    'fraction': [0.99, 0.01],
-    'seed': 12345,
-}
-training_params = [
-    {'learning_rate': 0.01, 'epochs':10},
-    {'learning_rate': 0.001, 'epochs':20},
+models_conf = [
+    {
+        'type': 'gmm',
+        'bayesian': False,
+        'ensembles': 1,
+        'centers': 5,
+        'hidden_sizes': (20, 50, 20),
+        'condition_labels' : condition_labels,
+        'y_label' : y_label,
+        'training_params': training_params,
+    },
+    {
+        'type':'gmevm',
+        'bayesian':False,
+        'ensembles':1,
+        'centers':4,
+        'hidden_sizes':(20, 50, 20),
+        'condition_labels' : condition_labels,
+        'y_label' : y_label,
+        'training_params': training_params,
+    },
 ]
 
-# open the dataframe from parquet files
-project_folder = "projects/tail_benchmark/" 
+# open the dataset
+project_folder = "projects/tail_benchmark/"
 project_paths = [project_folder+name for name in os.listdir(project_folder) if os.path.isdir(os.path.join(project_folder, name))]
 
-# limit simulation folder
-#project_paths = ['projects/tail_benchmark/p4_results']
+#project_paths = [
+#    'projects/tail_benchmark/p1_results',
+#]
 
 for project_path in project_paths:
-    logger.info(f"Starting simulation path: {project_path}")
+    logger.info(f"Openning the path '{project_path}'")
     
     predictors_path = project_path + '/predictors/'
     os.makedirs(predictors_path, exist_ok=True)
@@ -76,78 +100,84 @@ for project_path in project_paths:
     # read all files into Spark df
     df=spark.read.parquet(*files)
 
-    # take desired number of records for learning
-    df = df.sample(
-        withReplacement=False, 
-        fraction=dataset_size/df.count(),
-    )
-    df_train, df_val = df.randomSplit(train_val_split['fraction'], seed=train_val_split['seed'])
 
-    # dataset partitioning and making the converters
-    # Make sure the number of partitions is at least the number of workers which is required for distributed training.
-    NUM_WORKERS = 1
-    df_train = df_train.repartition(NUM_WORKERS)
-    df_val = df_val.repartition(NUM_WORKERS)
-    converter_train = make_spark_converter(df_train)
-    converter_val = make_spark_converter(df_val)
-    logger.info(f"Dataset loaded, train sampels: {len(converter_train)}, validation samples: {len(converter_val)}")
+    for model_conf in models_conf:
 
-    def transform_row(pd_batch):
-        """
-        The input and output of this function are pandas dataframes.
-        """
-        
-        pd_batch = pd_batch[[y_label,*condition_labels]]
-        pd_batch['y_input'] = pd_batch[y_label]
-        pd_batch = pd_batch.drop(columns=[y_label])
-        return pd_batch
+        training_params = model_conf['training_params']
 
-    # Note that the output shape of the `TransformSpec` is not automatically known by petastorm, 
-    # so we need to specify the shape for new columns in `edit_fields` and specify the order of 
-    # the output columns in `selected_fields`.
-    x_fields = [ (cond, npdtype, (), False) for cond in condition_labels ]
-    transform_spec_fn = TransformSpec(
-        transform_row,
-        edit_fields = [
-            *x_fields,
-            ('y_input', npdtype, (), False),
-        ],
-        selected_fields=[*condition_labels,'y_input'],
-    )
+        # take the desired number of records for learning
+        df = df.sample(
+            withReplacement=False, 
+            fraction=training_params['dataset_size']/df.count(),
+        )
+        df_train, df_val = df.randomSplit(
+            training_params['train_val_split']['fraction'], 
+            seed=training_params['train_val_split']['seed'],
+        )
 
-    for num_ensemble in range(ensembles):
-        
-        # limit
-        #num_ensemble=2 
+        # dataset partitioning and making the converters
+        # Make sure the number of partitions is at least the number of workers which is required for distributed training.
+        df_train = df_train.repartition(1)
+        df_val = df_val.repartition(1)
+        converter_train = make_spark_converter(df_train)
+        converter_val = make_spark_converter(df_val)
+        logger.info(f"Dataset loaded, train sampels: {len(converter_train)}, validation samples: {len(converter_val)}")
 
-        for model_name in model_names:
+        def transform_row(pd_batch):
+            """
+            The input and output of this function are pandas dataframes.
+            """
+            
+            pd_batch = pd_batch[[y_label,*condition_labels]]
+            pd_batch['y_input'] = pd_batch[y_label]
+            pd_batch = pd_batch.drop(columns=[y_label])
+            return pd_batch
+
+        # Note that the output shape of the `TransformSpec` is not automatically known by petastorm, 
+        # so we need to specify the shape for new columns in `edit_fields` and specify the order of 
+        # the output columns in `selected_fields`.
+        x_fields = [ (cond, npdtype, (), False) for cond in condition_labels ]
+        transform_spec_fn = TransformSpec(
+            transform_row,
+            edit_fields = [
+                *x_fields,
+                ('y_input', npdtype, (), False),
+            ],
+            selected_fields=[*condition_labels,'y_input'],
+        )
+
+        model_type = model_conf['type']
+        ensembles = model_conf['ensembles']
+        condition_labels = model_conf['condition_labels']
+        training_rounds = training_params['training_rounds']
+        batch_size = training_params['batch_size']
+        for num_ensemble in range(ensembles):
 
             # initiate the non conditional predictor
-            if model_name == 'gmm':
+            if model_type == 'gmm':
                 model = ConditionalGaussianMM(
-                    x_dim=condition_labels,
-                    centers= 5,
-                    hidden_sizes = (20, 50, 20),
+                    x_dim = condition_labels,
+                    centers = model_conf['centers'],
+                    hidden_sizes = model_conf['hidden_sizes'],
                     dtype = strdtype,
-                    bayesian = False,
+                    bayesian = model_conf['bayesian'],
                     #batch_size = 1024,
                 )
-            elif model_name == 'gevm':
+            elif model_type == 'gevm':
                 model = ConditionalGammaEVM(
-                    x_dim=condition_labels,
-                    centers=4,
-                    hidden_sizes = (20, 50, 20),
+                    x_dim = condition_labels,
+                    hidden_sizes = model_conf['hidden_sizes'],
                     dtype = strdtype,
-                    bayesian = False,
+                    bayesian = model_conf['bayesian'],
                     #batch_size = 1024,
                 )
-            elif model_name == 'gmevm':
+            elif model_type == 'gmevm':
                 model = ConditionalGammaMixtureEVM(
-                    x_dim=condition_labels,
-                    centers=4,
-                    hidden_sizes = (20, 50, 50, 20),
+                    x_dim = condition_labels,
+                    centers = model_conf['centers'],
+                    hidden_sizes = model_conf['hidden_sizes'],
                     dtype = strdtype,
-                    bayesian = False,
+                    bayesian = model_conf['bayesian'],
                     #batch_size = 1024,
                 )
 
@@ -174,8 +204,8 @@ for project_path in project_paths:
                 val_dataset = val_dataset.map(map_fn)
                 validation_steps = max(1, len(converter_val) // batch_size)
 
-                for idx, params in enumerate(training_params):
-                    logger.info(f"Starting training session {idx}/{len(training_params)} with {params}")
+                for idx, params in enumerate(training_rounds):
+                    logger.info(f"Starting training session {idx}/{len(training_rounds)} with {params}")
 
                     model._pl_training_model.compile(
                         optimizer=keras.optimizers.Adam(learning_rate=params['learning_rate']), 
@@ -193,6 +223,9 @@ for project_path in project_paths:
                         verbose=1,
                     )
 
-                model.save(predictors_path+f"{model_name}_model_{num_ensemble}.h5")
+            model.save(predictors_path+f"{model_type}_model_{num_ensemble}.h5")
+            with open(predictors_path+f"{model_type}_model_{num_ensemble}.json", "w") as write_file:
+                json.dump(model_conf, write_file, indent=4)
 
-                logger.info(f"Model {num_ensemble} saved, bayesian: {model.bayesian}, batch_size: {model.batch_size}")
+            logger.info(f"A {model_type} {'non-bayesian' if model.bayesian else 'bayesian'} " + \
+                f"model got trained and saved, ensemble: {num_ensemble}.")
